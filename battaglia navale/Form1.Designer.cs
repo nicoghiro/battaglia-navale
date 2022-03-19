9c4bef75f3fd926d02a45444072a90a299d0748f
nicoghiro <nicolasghirardi251005@gmail.com>
2022-03-19 22:59:47 +0100

    prova battaglia delle palle

--- battaglia navale/Form1.Designer.cs
+++ battaglia navale/Form1.Designer.cs
@@ -38,6 +38,7 @@ namespace battaglia_navale
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(198, 22);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
--- battaglia navale/Form1.cs
+++ battaglia navale/Form1.cs
@@ -10,8 +10,11 @@ using System.Windows.Forms;
 
 namespace battaglia_navale
 {
+    
+
     public partial class Form1 : Form
     {
+      
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +22,54 @@ namespace battaglia_navale
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
 
+            
+           // nave [,] suca = Popi(a);
+        }
+        public nave[,] Popi(int grandezza)
+        {
+            int a = 197, b = 170;
+            nave[,] popi = new nave[grandezza, grandezza];
+            for(int i=0;i < grandezza; i++) { 
+            for(int j=0;j< grandezza; j++) { 
+            Button button1 = new Button();
+            this.Controls.Add(button1);
+            button1.Location=new Point(a, b);
+                    button1.Size = new Size(20, 20);
+                a = a + 20;
+                 
+             }
+                a = 197;
+                b = b + 20;
+            }
+            return popi;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int a = 0;
+            string ugg;
+            ugg = textBox1.Text;
+            char[] pippo = ugg.ToCharArray();
+            
+                if (pippo.Length < 2) { 
+                a = ((int)pippo[0] - 48);
+                }
+                if(pippo.Length == 2 )
+                {
+                a = (((int)pippo[0] - 48) * 10) + (((int)pippo[1] - 48));
+                }
+            if(pippo.Length>3)
+            {
+                textBox1.Text = "tabella troppo grande ( < 100) ";
+            }
+            
+            nave [,] suca = Popi(a);
         }
     }
 }
-static void 
+
 
 
 
